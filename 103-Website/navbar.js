a8e180c72bb01ed9e52ec78fcfa486e8edec0e6c
KazuOfficial <kazuofficial.contact@gmail.com>
2021-04-05 18:40:51 +0200

    103 Website - Navbar Fix

--- 103-Website/navbar.js
+++ 103-Website/navbar.js
@@ -1,17 +1,23 @@
-const checkpoint = 320;
-const checkpoint2 = 1300;
-
 window.addEventListener("scroll", () => {
   const currentScroll = window.pageYOffset;
   var w = $(window).width();
+  if (currentScroll > 200) {
+    if (!$("header").hasClass("expand")) {
+      $("header").addClass("expand");
+    }
+  } else {
+    if ($("header").hasClass("expand")) {
+      $("header").removeClass("expand");
+    }
+  }
   if (w > 1150) {
-    if (currentScroll <= checkpoint) {
+    if (currentScroll <= 320) {
       $(".youtube-section").stop().fadeOut(1);
     } else {
       $(".youtube-section").fadeIn(500);
     }
 
-    if (currentScroll <= checkpoint2) {
+    if (currentScroll <= 1300) {
       $(".circle-section").stop().fadeOut(1);
     } else {
       $(".circle-section").fadeIn(500);
