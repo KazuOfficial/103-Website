ee574e5495363de44f0188ca381933d613217c0e
KazuOfficial <kazuofficial.contact@gmail.com>
2021-04-05 18:36:22 +0200

    103 Website Animations

--- 103-Website/navbar.js
+++ 103-Website/navbar.js
@@ -1,11 +1,29 @@
-$(window).on('scroll', function () {
-    if ($(this).scrollTop() > 200) {
-        if (!$('header').hasClass('expand')) {
-            $('header').addClass('expand');
-        }
+const checkpoint = 320;
+const checkpoint2 = 1300;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  var w = $(window).width();
+  if (w > 1150) {
+    if (currentScroll <= checkpoint) {
+      $(".youtube-section").stop().fadeOut(1);
     } else {
-        if ($('header').hasClass('expand')) {
-            $('header').removeClass('expand');
-        }
+      $(".youtube-section").fadeIn(500);
     }
+
+    if (currentScroll <= checkpoint2) {
+      $(".circle-section").stop().fadeOut(1);
+    } else {
+      $(".circle-section").fadeIn(500);
+    }
+  } else {
+    $(".youtube-section").fadeIn(1);
+    $(".circle-section").fadeIn(1);
+  }
+});
+
+$(document).ready(function () {
+  $("body").fadeIn(500);
+  $(".youtube-section").fadeOut(1);
+  $(".circle-section").fadeOut(1);
 });
